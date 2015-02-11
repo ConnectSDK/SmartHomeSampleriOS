3cb9a41ee7682c640089c2fc270b1e5322b3cc87
Ibrahim Adnan <ibrahim.adnan@lge.com>
2015-02-10 17:07:08 -0800

    Adding Single view application

---
+++ SmartHomeSampleriOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHomeSampleriOS
+//
+//  Created by Ibrahim Adnan on 2/10/15.
+//  Copyright (c) 2015 Ibrahim Adnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartHomeSampleriOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SmartHomeSampleriOS
+//
+//  Created by Ibrahim Adnan on 2/10/15.
+//  Copyright (c) 2015 Ibrahim Adnan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
